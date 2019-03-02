52d16353ae8b8da01ee5ef9c82423c7c931a780e
Chris Jennings <chrisjennings555@gmail.com>
2019-03-02 16:22:53 +0000

    initial files setup

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import TitleBanner from '../components/TitleBanner.js';
+import MainContent from '../components/MainContent.js';
+import FooterBar from '../components/FooterBar.js';
+
+
+export default MainContainer;
